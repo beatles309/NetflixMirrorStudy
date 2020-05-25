3887d0560fe553e327db6650c219464deedeca82
Sangwook-Ahn <tkddnr924@naver.com>
2020-05-26 01:28:15 +0900

    webpack update & webpack-dev-server update & html-webpack-plugin update

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ import router from './router'
 Vue.config.productionTip = false
 
 // bootstrap & jquery
+/* eslint-disable */
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.css'
 
-import 'jquery/dist/jqeury.js'
+import 'jquery/dist/jquery.js'
+import 'popper.js/dist/popper.js'
 
 /* eslint-disable no-new */
 new Vue({

2238de911eed62de97381a80c2af5c7d74b00a32
jinhyang <beatles309@naver.com>
2020-05-29 22:45:27 +0900

    Store(Vuex) 의존성 추가 및 초기 설정

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 
+Vue.use(Vuex)
+
 export default new Vuex.Store({
-    modules: {
-        App
-    }
+  modules: {
+    App
+  }
 })

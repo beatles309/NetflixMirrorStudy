7893f5bab4df87df2d592a857123ab5d568e6b16
Sangwook-Ahn <tkddnr924@naver.com>
2020-06-25 01:09:55 +0900

    api 추가

--- src/common/api/moviedb.js
+++ src/common/api/moviedb.js
@@ -3,6 +3,7 @@ import { API_KEY } from '@/constants'
 
 /* eslint-disable */
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500'
-export const getMovieGenreList = (language) => get(`/3/genre/movie/list?api_key=${API_KEY}&language=${ language }`, {})
-export const getMovieTopRated = (language, page) => get(`/3/movie/top_rated?api_key=${API_KEY}&language=${language}&page=${page}`, {})
-export const getMovieDetail = (language, movieID) => get(`/movie/${movieID}?api_key=${API_KEY}&language=${language}`, {})
+export const getMovieGenreList = (language) => get(`/3/genre/movie/list?api_key=${API_KEY}&language=${ language }`)
+export const getMovieTopRated = (language, page) => get(`/3/movie/top_rated?api_key=${API_KEY}&language=${language}&page=${page}`)
+export const getMovieDetail = (language, movieID) => get(`/movie/${movieID}?api_key=${API_KEY}&language=${language}`)
+export const getMoviePopular = (language, page) => get(`/3/movie/popular?api_key=${API_KEY}&language=${language}&page=${page}`)
